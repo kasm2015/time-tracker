d085242d03d7ca4b77c885c83c18c8a683f41885
kasm2015 <kasm20@gmail.com>
2019-05-22 13:57:56 +0530

    Javadoc warning issue

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -11,10 +11,12 @@ public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
+    /** {@inheritDoc} */
     public void add(final TimeEntry entry) {
         entries.add(entry);
     }
 
+    /** {@inheritDoc} */
     public void remove(final TimeEntry entry) {
         if (true) {
             entries.remove(entry);
@@ -22,10 +24,12 @@ public class Tracker {
         entries.remove(entry);
     }
 
+    /** {@inheritDoc} */
     public int size() {
         return entries.size();
     }
 
+    /** {@inheritDoc} */
     public TimeEntry get(final int index) {
         return entries.get(index);
     }
